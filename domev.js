50351ab912ff718cf10350e7d93556ea4cee1994
iambumblehead <cdepauw@gmail.com>
2017-04-27 13:30:47 -0700

    Update domev.js

--- domev.js
+++ domev.js
@@ -1,64 +1,47 @@
 // Filename: domev.js
-// Timestamp: 2016.11.07-12:14:20 (last modified)
+// Timestamp: 2017.04.26-21:28:17 (last modified)
 // Author(s): Bumblehead (www.bumblehead.com)
 
-var domev = module.exports = {
+const domev = module.exports = (o => {
   
-  getElemAt : function (e) {
-    var fn = function () {};
+  o.getElemAt = (e, fn = ev => null) => {
     if (typeof e === 'object' && e) {
-      if ('target' in e) {
-        fn = function (ev) {
-          return ev.target;
-        };
-      } else if ('srcElement' in e) {
-        fn = function (ev) {
-          return ev.srcElement;
-        };
+      if ('srcElement' in e) {
+        fn = ev => ev.srcElement;        
+      } else if ('target' in e) {
+        fn = ev => ev.target;
       }
     }
-    return (domev.getElemAt = fn)(e);
-  },
 
-  getparentlinkelemat : function (e) {
-    var elem = this.getElemAt(e);
+    return (o.getElemAt = fn)(e);
+  };
 
-    return elem && (function getparentlink (elem) {
-      return (elem && elem.tagName) && (
-        elem.tagName.match(/^a/i)
-          ? elem : getparentlink(elem.parentNode));        
-    }(elem));
-  },
-
-  stopDefaultAt : function (e) {
-    var fn = function () {};
+  o.stopDefaultAt = (e, fn = ev => null) => {
     if (typeof e === 'object' && e) {
-      if (e.preventDefault) {
-        fn = function (ev) {
-          return ev.preventDefault();          
-        };
+      if (typeof e.preventDefault) {
+        fn = ev => (ev.preventDefault());
       } else {
-        fn = function (ev) {
-          return ev.returnValue = false;          
-        };
+        fn = ev => (ev.returnValue = false);
       }
     }
-    return (domev.stopDefaultAt = fn)(e);
-  },
+    
+    return (o.stopDefaultAt = fn)(e);
+  };
 
-  isElem : function (e, elem, evelem) {
-    evelem = this.getElemAt(e, elem);
-
-    return elem && evelem 
-      && elem.isEqualNode(evelem);
-  },
+  o.getparentlinkelemat = (e, elem = o.getElemAt(e)) => (
+    elem && (function getparentlink (elem) {
+      return (elem && elem.tagName) && (
+          /^a/i.test(elem.tagName)
+          ? elem : getparentlink(elem.parentNode));
+    }(elem)));
   
-  hasElem : function (e, elem, evelem) {
-    evelem = this.getElemAt(e, elem);
+  o.isElem = (e, elem, evelem = o.getElemAt(e, elem)) => (
+    elem && evelem && elem.isEqualNode(evelem));
+  
+  o.hasElem = (e, elem, evelem = o.getElemAt(e, elem)) => (
+    elem && evelem && (elem.isEqualNode(evelem) ||
+                       elem.contains(evelem)));
 
-    return elem && evelem 
-      && (elem.isEqualNode(evelem) || elem.contains(evelem));  
-  }
-};
-
-
+  return o;
+  
+})({});
